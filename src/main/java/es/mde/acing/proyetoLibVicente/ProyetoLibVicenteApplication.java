e73837087bf09a9297bf0a475b5b3f479fee1ce3
Diego Vicente <dvicentejua@gmail.com>
2023-03-16 12:01:16 +0100

    ---Ejercicio nº 2---

---
+++ src/main/java/es/mde/acing/proyetoLibVicente/ProyetoLibVicenteApplication.java
@@ -0,0 +1,13 @@
+package es.mde.acing.proyetoLibVicente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyetoLibVicenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyetoLibVicenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/mde/acing/proyetoLibVicente/ProyetoLibVicenteApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mde.acing.proyetoLibVicente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyetoLibVicenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
